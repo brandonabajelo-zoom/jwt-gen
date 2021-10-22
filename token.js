e7d891017f06e1bf4ee8d025f0909cfafa51bd6e
brandonabajelo-zoom <brandon.abajelo@zoom.us>
2021-10-22 11:22:25 -0700

    add cli for env creation

---
+++ token.js
@@ -0,0 +1,49 @@
+import env from 'dotenv';
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+import prompt from 'prompt';
+import { spawn } from 'child_process';
+
+const hasEnvironmentFile = fs.existsSync('./.env');
+
+const generateToken = (key = '', secret = '') => jwt.sign({ iss: key, exp: new Date().getTime() + 7200000 }, secret);
+
+const copyToken = token => {
+  const cmd = spawn('pbcopy');
+  cmd.stdin.write(token);
+  cmd.stdin.end();
+
+  console.log(`JWT copied to clipboard: ${token}`);
+}
+
+if (hasEnvironmentFile) {
+  env.config();
+
+  const { API_KEY, API_SECRET } = process.env;
+  const jwtToken = generateToken(API_KEY, API_SECRET);
+
+  copyToken(jwtToken);
+
+} else {
+  const inputSchema = {
+    properties: {
+      'Zoom API Key': { required: true },
+      'Zoom API Secret': { required: true },
+    }
+  };
+
+  prompt.start();
+  prompt.get(inputSchema, (err, input) => {
+    const key = input['Zoom API Key'];
+    const secret = input['Zoom API Secret'];
+
+    fs.writeFile('.env', `API_KEY=${key}\nAPI_SECRET=${secret}`, writeError => {
+      if (writeError) throw writeError
+      console.log('Environment file created with Zoom Credentials');
+
+      const token = generateToken(key, secret);
+      copyToken(token);
+    });
+
+  })
+}
